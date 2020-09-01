4140f185a8d9f4b603c452f5de7b376d37209a54
Leteno <changchunjunhao@gmail.com>
2020-09-01 21:36:41 +0800

    Setup Grammar test

--- parser/Grammar.py
+++ parser/Grammar.py
@@ -1,3 +1,19 @@
 
+import json, sys
+
 def buildAST(tokens):
     pass
+
+def test():
+    f = open('Student.tokenizer.json', 'r')
+    tokens = json.load(f)
+    f.close()
+    f = open('Student.grammar.json', 'r')
+    expect = json.load(f)
+    f.close()
+    ast = buildAST(tokens)
+    assert expect == ast, "expect: %s, actual: %s" % (expect, ast)
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(test())
