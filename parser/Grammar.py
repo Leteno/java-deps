0026b86dab4fba07b1433194adb8d8241795f2ca
Leteno <changchunjunhao@gmail.com>
2020-09-09 21:42:00 +0800

    Grammar: Support class

--- parser/Grammar.py
+++ parser/Grammar.py
@@ -15,9 +15,15 @@ class ASTBuilder:
         assert self.index == self.size, "break at %s, %s" % (self.index, self.size)
         return ret
 
-    def statements(self, meetEndAsPossible=False):
+    '''
+    TODO: remove stopToken when FSM is stable.
+    '''
+    def statements(self, meetEndAsPossible=False, stopTokens=[]):
         stmts, oldIndex = [], self.index
         while self.index < self.size:
+            if self.currentType() in stopTokens or \
+                self.currentValue() in stopTokens:
+                break
             stmt = self.statement()
             if not stmt:
                 if meetEndAsPossible:
@@ -37,9 +43,31 @@ class ASTBuilder:
             return self.packageClause()
         elif currentValue == "import":
             return self.importClause()
-        else:
-            # TODO skip it by now
+
+        # unset to avoid bug
+        currentType, currentValue = None, None
+
+        oldIndex = self.index
+        AccessModifier, Static, Final = None, None, None
+        if self.currentType() == "access-modifier":
+            AccessModifier = self.currentToken()
             self.index += 1
+            assert self.index < self.size
+        if self.currentType() == "static":
+            Static = self.currentToken()
+            self.index += 1
+            assert self.index < self.size
+        if self.currentType() == "final":
+            Static = self.currentToken()
+            self.index += 1
+            assert self.index < self.size
+
+        currentType, currentValue = self.currentType(), self.currentValue()
+        if self.currentType() in ["class", "interface"]:
+            # class or interface = =)
+            return self.classClause(AccessModifier, Static, Final)
+        # TODO skip it by now
+        self.index += 1
 
     def packageClause(self):
         self.index += 1 # package
@@ -89,6 +117,50 @@ class ASTBuilder:
                 'path': None
             }
         return retClassPath
+
+    def classClause(self, AccessModifier, Static, Final):
+        assert self.currentType() in ["class", "interface"]
+        classType = self.currentToken()
+        self.index += 1
+        assert self.currentType() == "variable", "fail at %s" % self.index
+        className = self.currentToken()
+        self.index += 1
+        Extends, Implements = None, []
+        if self.currentType() == "extends":
+            self.index += 1
+            Extends = self.classPathExpr()
+            assert Extends, "extends is null at: %s" % self.index
+        if self.currentType() == "implements":
+            self.index += 1
+            Implements = self.classListExpr()
+            assert Implements, "implements is null at: %s" % self.index
+        assert self.currentValue() == "{"
+        self.index += 1
+        ClassStatements = self.statements(meetEndAsPossible=True, stopTokens=["}"])
+        assert self.currentValue() == "}"
+        self.index += 1
+        return {
+            "type": "class-clause",
+            "class-type": classType, # class, interface
+            "className": className,
+            "access-modifier": AccessModifier,
+            "static": Static,
+            "final": Final,
+            "extends": Extends,
+            "implements": Implements,
+            "class-statements": ClassStatements
+        }
+
+    def classListExpr(self):
+        ClassList = []
+        Class = self.classPathExpr()
+        assert Class
+        ClassList.append(Class)
+        while self.currentValue() == ",":
+            Class = self.classPathExpr()
+            assert Class
+            ClassList.append(Class)
+        return ClassList
 
     def getToken(self, index):
         return self.tokens[index]
